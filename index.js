dde0adb234083a7682d0e7ca172c4e8a1c759993
rowlandwilliams <Rowland@Matthews-MacBook-Air.local>
2020-11-02 20:51:56 +0000

    added intial mouseover functionality

--- index.js
+++ index.js
@@ -81,17 +81,19 @@ d3.csv('./bft_data.csv').then(function(data) {
         }))
     }))
 
+    // set random colours
     var color = [... new Set(nodes.map(x => x.loc))].map(y => ({'loc': y, 'col': '#' + Math.floor(Math.random()*16777215).toString(16)}))
 
 
-
+    // dimensions
     var width = window.innerWidth;
     var height = window.innerHeight;
     var radius = width / 2;
     tree = d3.cluster()
         .size([2 * Math.PI, radius - 100])
 
-
+    
+    // define line, control bundle with beta
     line = d3.lineRadial()
         .curve(d3.curveBundle.beta(0.85))
         .radius(d => d.y)
@@ -102,18 +104,19 @@ d3.csv('./bft_data.csv').then(function(data) {
     colorout = "#f00"
     colorin = "#00f"
 
-
+    // function to identify id of linked components
     function id(node) {
         return `${node.parent ? id(node.parent) + "." : ""}${node.data.name}`;
       }
 
+    // account for incoming (not defined in imports) and outgoing (imports)
     function bilink(root) {
         const map = new Map(root.leaves().map(d => [id(d), d]));
+        
         for (const d of root.leaves()) d.incoming = [], d.outgoing = d.data.imports.map(i => [d, map.get(i)]);
         for (const d of root.leaves()) for (const o of d.outgoing) o[1].incoming.push(o);
         return root;
       }
-
 
 
     data = nest;
@@ -121,12 +124,15 @@ d3.csv('./bft_data.csv').then(function(data) {
     const root = tree(bilink(d3.hierarchy(data)
       .sort((a, b) => d3.ascending(a.height, b.height) || d3.ascending(a.data.name, b.data.name))));
 
-    console.log(color)
+      var test = d3.hierarchy(data)
+      console.log(test.leaves())
+
 
     const svg = d3.select('body').append("svg")
       .attr("viewBox", [-width  , -height, width * 2, height * 2]);
 
-    const node = svg.append("g")
+    // add nodes
+      const node = svg.append("g")
         .attr("font-family", "sans-serif")
         .attr("font-size", 20)
         .attr('stroke', '#202020')
@@ -139,10 +145,10 @@ d3.csv('./bft_data.csv').then(function(data) {
         .attr("x", d => d.x < Math.PI ? 6 : -6)
         .attr("text-anchor", d => d.x < Math.PI ? "start" : "end")
         .attr("transform", d => d.x >= Math.PI ? "rotate(180)" : null)
-        .text(function(d){ console.log(d); return nodes.filter(x => x.xid == d.data.name)[0].name})
+        .text(function(d){ return nodes.filter(x => x.xid == d.data.name)[0].name})
         .each(function(d) { d.text = this; })
-//       .on("mouseover", overed)
-//       .on("mouseout", outed)
+        .on("mouseover", overed)
+        .on("mouseout", outed)
 //       .call(text => text.append("title").text(d => `${id(d)}
 // ${d.outgoing.length} outgoing
 // ${d.incoming.length} incoming`));
@@ -159,5 +165,33 @@ d3.csv('./bft_data.csv').then(function(data) {
         .attr("d", ([i, o]) => line(i.path(o)))
         .each(function(d) { d.path = this; });
     // var sizes = {'very small': 3, 'small': 4, 'medium': 5, 'medium/large': 5, 'large': 6}
+
+    function overed(d) {
+        // console.log(d)
+        console.log(d)
+        link.style("mix-blend-mode", null);
+        d3.select(this).attr('font-weight', 'bold')
+            .transition().duration(500).delay(500)
+        d3.selectAll(d.incoming.map(d => d.path)).attr("stroke", colorin).raise();
+        d3.selectAll(d.incoming.map(([d]) => d.text)).attr("fill", colorin).attr("font-weight", "bold");
+        d3.selectAll(path).attr("stroke", 'grey');
+
+        d3.selectAll(d.outgoing.map(d => d.path)).attr("stroke", colorin).raise();
+        d3.selectAll(d.outgoing.map(([, d]) => d.text)).attr("fill", colorin).attr("font-weight", "bold");
+      }
+    
+
+      function outed(d) {
+        // console.log(d)
+        d3.select(this).attr('font-weight', 'null')
+
+        
+        link.style("mix-blend-mode", 'multiply');
+        d3.selectAll(d.incoming.map(d => d.path)).attr("stroke", null);
+        d3.selectAll(d.incoming.map(([d]) => d.text)).attr("fill", null).attr("font-weight", null);
+        d3.selectAll(d.outgoing.map(d => d.path)).attr("stroke", null);
+        d3.selectAll(d.outgoing.map(([, d]) => d.text)).attr("fill", null).attr("font-weight", null);
+  
+      }
 
     })
